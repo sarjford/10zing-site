d7229383b06d0369e03a8a2288918db991615c83
Sarah Ford <sarah.jeong.ford@gmail.com>
2018-01-21 14:13:09 -0800

    home done, migrate css

--- client/index.js
+++ client/index.js
@@ -13,12 +13,17 @@ $(document).ready(function(){
     $('.navigation').before('<img src="/assets/orange_logo.png">');
     $('.diagonal.section4 .content').after('<img class="climbers-chart-mobile" src="/assets/Climbers_Chart_MobileHome.png">');
 
+    $('.section3 h2').before('<img class="seo-header" src="/assets/seo_h1_mobile.png">');
+
+
     $('.navigation li:first-child').click(function(){
       if (!$(this).hasClass('services-expanded')) {
+        $('.navigation li').css('padding-bottom', '9px');
         $('header ul ul li').css('color', '#fff');
         $('#services').show();
         $(this).addClass('services-expanded');
       } else {
+        $('.navigation li').css('padding-bottom', '20px');
         $('#services').hide();
         $(this).removeClass('services-expanded');
       }
@@ -33,10 +38,18 @@ $(document).ready(function(){
     $('.diagonal.section4 .content').after('<img class="climbers-img" src="/assets/climbers_fromside.png"><img class="chart-img" src="/assets/chart.png">');
     // $('.section5 .content').before('<img src="/assets/climber_lookingout_backdrop.png">');
 
+
+
+    $('.section3 h2').before('<img src="/assets/orange_key.png">');
+
+
+
     // handler for marketing services nav list
     $('.navigation li:first-child').hover(function(){
       $('#services').show();
-      $(this).addClass('hover-over-services');
+      $(this).children('div').addClass('hover-over-services');
+      $(this).find('ul').addClass('hover-over-services-ul');
+
 
       // marketing services sub-nav
       $('#services li').hover(function(){
@@ -50,7 +63,8 @@ $(document).ready(function(){
 
       $(this).mouseleave(function(){
         $('#services').hide();
-        $(this).removeClass('hover-over-services');
+        $(this).children('div').removeClass('hover-over-services');
+        $(this).find('ul').removeClass('hover-over-services-ul');
       });
     });
   }
